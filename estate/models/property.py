53d3ece34c7a61aa69f18e025b30f8c3e3c30ba8
Ilia Davydov <ilia.v.davydov@gmail.com>
2023-11-06 11:15:41 +0100

    update property models

--- estate/models/property.py
+++ estate/models/property.py
@@ -1,5 +1,6 @@
 from odoo import fields, models
 
+
 class EstateProperty(models.Model):
     _name = "estate_property"
     _description = "Properties of the estate"
@@ -8,3 +9,14 @@ class EstateProperty(models.Model):
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date()
+    expected_price = fields.Float()
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Selection(
+        string='Type',
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+        help="Garden Area orientation")
