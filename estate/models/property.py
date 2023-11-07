b010fb48a7d1dfb95aad674b153bd277a120a415
Ilia Davydov <ilia.v.davydov@gmail.com>
2023-11-07 11:44:30 +0100

    ch9: add computed

--- estate/models/property.py
+++ estate/models/property.py
@@ -38,8 +38,8 @@ class EstateProperty(models.Model):
     property_type_id = fields.Many2one('estate_property_type')
     buyer_id = fields.Many2one('res.users')
     seller_id = fields.Many2many('res.partner')
-    total_area = fields.Float(compute="_compute_total_area", store=True)
-
+    # total_area = fields.Float(compute="_compute_total_area", store=True)
+    test = fields.Integer()
     @api.depends("living_area", "garden_area")
     def _compute_total_area(self):
         for record in self:
